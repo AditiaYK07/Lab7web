a2c259ea38ec24a3b7db1c8523e216d826d92c73
AditiaYK07 <aditiayudakartika@gmail.com>
2022-11-20 21:56:21 +0700

    commit pertama

---
+++ php_dasar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar </h1>
+    <?php
+        echo "Hello Word" ;
+    ?>
+    <h2> Menggunakan Variable </h2>
+    <?php
+    $nim = "312110334";
+    $nama = "Aditia Yuda Kartika" ;
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+
+    
+</body>
+</html>
